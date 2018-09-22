765da65a07add5d34e112e85abab542a7740edf3
Mark Jung <gujung2022@u.northwestern.edu>
2018-09-22 18:52:37 -0500

    forgot to add seconds

--- utils/bodybuilder.py
+++ utils/bodybuilder.py
@@ -3,6 +3,28 @@ class BodyBuilder():
     Constructs the html body of the email
     """
     @classmethod
+    def alert_feedback(cls, record_url, record_duration, member_name, fam_id):
+        body = """
+        <html>
+            <body style="text-align:center; font-size: 18px; font-family: 'Times New Roman', Times, serif;">
+                <h3>A feedback has been given by {member_name}!</h3>
+                <div style="margin-top: 3px;">Feedback Url: {record_url} </div>
+                <div style="margin-top: 3px;">Feedback Duration: {record_duration} Seconds </div>
+                <div style="margin-top: 3px;">From: {member_name} </div>
+                <div style="margin-top: 3px;">Family ID: {fam_id} </div>
+                <br>
+                <div>Thank you</div>
+                <br>
+                <hr style="width:50%;">
+                <br>
+                <div>Team Niche</div>
+                <br>
+            </body>
+        </html>
+        """.format(member_name=member_name, fam_id=fam_id, record_url=record_url, record_duration=record_duration)
+        return body 
+
+    @classmethod
     def alert_order(cls, total, member_name, family_name, items, checkout_id):
         body = """
         <html>
@@ -35,28 +57,6 @@ class BodyBuilder():
         </html>"""
         return body 
     
-    @classmethod
-    def alert_feedback(cls, record_url, record_duration, member_name, fam_id):
-        body = """
-        <html>
-            <body style="text-align:center; font-size: 18px; font-family: 'Times New Roman', Times, serif;">
-                <h3>A feedback has been given by {member_name}!</h3>
-                <div style="margin-top: 3px;">Feedback Url: {record_url} </div>
-                <div style="margin-top: 3px;">Feedback Duration: {record_duration} </div>
-                <div style="margin-top: 3px;">From: {member_name} </div>
-                <div style="margin-top: 3px;">Family ID: {fam_id} </div>
-                <br>
-                <div>Thank you</div>
-                <br>
-                <hr style="width:50%;">
-                <br>
-                <div>Team Niche</div>
-                <br>
-            </body>
-        </html>
-        """.format(member_name=member_name, fam_id=fam_id, record_url=record_url, record_duration=record_duration)
-        return body 
-
     @classmethod
     def signup_createfam(cls, name, admin_invite, member_invite, fam_name):
         body = """
